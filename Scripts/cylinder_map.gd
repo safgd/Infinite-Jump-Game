ad65723ce92abc74ebd41847ec3fcf0489d779d0
123amir <safaei.a1@gmail.com>
2026-05-04 21:14:44 +0200

    platforms adjusted so moving them later will be easier

--- Scripts/cylinder_map.gd
+++ Scripts/cylinder_map.gd
@@ -3,14 +3,22 @@ extends Node3D
 @export var platform_scene: PackedScene
 @export var radius: float = 5.0
 @onready var platforms: Node3D = $Platforms
+@export var plat_dist_x: float = 40.0
+@export var plat_dist_y: float = 3.0
 
 func _ready() -> void:
-	for i: int in range(10):
-		spawn_platform()
+	var plat_rotation: float = 0.0
+	for i: int in range(50):
+		var plat: Node3D = spawn_platform(i * plat_dist_y)
+		if randf() < 0.5:
+			plat_rotation += plat_dist_x
+		else:
+			plat_rotation -= plat_dist_x
+		plat_rotation = floori(plat_rotation) % 360
+		plat.rotation_degrees.y = plat_rotation
 
-func spawn_platform()->void:
-	var plat = platform_scene.instantiate()
+func spawn_platform(pos_y: float)->Node3D:
+	var plat: Node3D = platform_scene.instantiate()
 	platforms.add_child(plat)
-	var direction: Vector2 = (Vector2(randi(), randi())).normalized() * radius
-	var y: float = randi() % 10
-	plat.global_position = Vector3(direction.x, y, direction.y)
+	plat.global_position.y = pos_y
+	return plat
